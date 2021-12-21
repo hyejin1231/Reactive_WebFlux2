b6b778ef79ebf1300d15c720965a5ebcd19a139f
hyejin1231 <phj981231@naver.com>
2021-12-21 11:19:23 +0900

    second commit

--- myspringboot-webflux-2dbc/src/main/java/myspringboot/reactive/myspringbootwebflux2dbc/h2/controller/R2CustomerController.java
+++ myspringboot-webflux-2dbc/src/main/java/myspringboot/reactive/myspringbootwebflux2dbc/h2/controller/R2CustomerController.java
@@ -3,9 +3,11 @@ package myspringboot.reactive.myspringbootwebflux2dbc.h2.controller;
 import myspringboot.reactive.myspringbootwebflux2dbc.h2.entity.Customer;
 import myspringboot.reactive.myspringbootwebflux2dbc.h2.repository.R2CustomerRepository;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 
 import java.time.Duration;
 
@@ -15,13 +17,34 @@ public class R2CustomerController {
 
     private final R2CustomerRepository customerRepository;
 
+    /*
+        조회요청 -> Flux
+        등록요청 -> Flux
+        Sinks는 Flux.merge 로 두개의 Flux를 합쳐주는 역할을 한다.
+     */
+    private final Sinks.Many<Customer> sinks;
+
     public R2CustomerController(R2CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
+        //Sinks.many() => Sinks.ManySpec
+        //Sinks.many().multicast() => Sinks.MulticastSpec
+        //Sinks.many().multicast().onBackpressureBuffer() => Sinks.Many
+        sinks = Sinks.many().multicast().onBackpressureBuffer();
     }
 
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Customer> findAllCustomer() {
         return customerRepository.findAll().delayElements(Duration.ofSeconds(1)).log();
+    }
+
+    //SeverSentEvent 객체를 직접 사용하면 MediaType.TEXT_EVENT_STREAM_VALUE 설정을 생략할 수 있다.
+    @GetMapping(value = "/sse")
+    public Flux<ServerSentEvent<Customer>> findAllCustomerSSE() {
+        return sinks.asFlux()
+                .mergeWith(customerRepository.findAll())
+                .map(customer -> ServerSentEvent.builder(customer).build())
+                .doOnCancel(() ->sinks.asFlux().blockLast());
+
     }
 
     @GetMapping("/{id}")
@@ -31,6 +54,9 @@ public class R2CustomerController {
 
     @PostMapping
     public Mono<Customer> saveCustomer(@RequestBody Customer customer) {
-        return customerRepository.save(customer);
+        //tryEmitNet : Try emitting a non-null element, generating an onNext signal
+        return customerRepository.save(customer).doOnNext(
+                insCust -> sinks.tryEmitNext(insCust)
+        ).log();
     }
 }
